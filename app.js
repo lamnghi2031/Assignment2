58f2bd2fa7b3c791ad1cdca311a5a1d3c53e5920
lamnghi2031 <84695003+lamnghi2031@users.noreply.github.com>
2021-06-27 19:06:47 +0700

    web

--- app.js
+++ app.js
@@ -40,7 +40,8 @@ app.use("/report", ReportController);
 //   });
 
 //add the router
-app.use('/', router);
-app.listen(process.env.port || 3000);
+const PORT = process.env.PORT || 3000;
 
-console.log('Running at Port 3000');
+app.listen(PORT);
+
+console.debug('Server is running ' + PORT);
